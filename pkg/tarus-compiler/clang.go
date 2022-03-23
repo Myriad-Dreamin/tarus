93fb0d9e2050c57105acc0dfa9e5e9168f5fe5b4
Myriad-Dreamin <camiyoru@gmail.com>
2022-03-24 01:51:57 +0800

    dev(compiler::Clang): basic compilation

--- pkg/tarus-compiler/clang.go
+++ pkg/tarus-compiler/clang.go
@@ -1,8 +1,11 @@
 package tarus_compiler
 
 import (
+	"fmt"
 	"github.com/containerd/containerd/errdefs"
+	"os/exec"
 	"path/filepath"
+	"strings"
 )
 
 type MultiVerCompiler struct {
@@ -37,11 +40,38 @@ type clangCompiler struct {
 	Clang string
 }
 
-func (g *clangCompiler) Compile(args *CompilerArgs) (CompilerResponse, error) {
-	return CompilerResponse{}, errdefs.ErrNotImplemented
+func (g *clangCompiler) Compile(args *CompilerArgs) (cr CompilerResponse, err error) {
+	var cmdArgs []string
+	for i := 0; i < len(args.Args); i++ {
+		if strings.HasPrefix(args.Args[i], "@") {
+			if i+1 == len(args.Args) {
+				err = fmt.Errorf("incompleted argument: %v", args.Args[i])
+				return
+			}
+			switch args.Args[i][1:] {
+			case "input":
+				i++
+				cmdArgs = append(cmdArgs, args.Args[i])
+			case "output":
+				i++
+				cmdArgs = append(cmdArgs, "-o", args.Args[i])
+			case "include_dir":
+				i++
+				cmdArgs = append(cmdArgs, "-I", args.Args[i])
+			default:
+				err = fmt.Errorf("unknown common option: %v", args.Args[i])
+				return
+			}
+		} else {
+			cmdArgs = append(cmdArgs, args.Args[i])
+		}
+	}
+
+	err = consumeCmd(exec.Command(g.Clang, cmdArgs...))
+	return
 }
 
-func (g clangCompiler) detect(s string) error {
+func (g *clangCompiler) detect(s string) error {
 	clang := filepath.Join(s, "bin/clang")
 	if exists(clang) {
 		_, err := output(clang, "--version")
--- pkg/tarus-compiler/utils.go
+++ pkg/tarus-compiler/utils.go
@@ -2,6 +2,7 @@ package tarus_compiler
 
 import (
 	"bytes"
+	"fmt"
 	"os"
 	"os/exec"
 )
@@ -14,21 +15,32 @@ func exists(fp string) bool {
 	return false
 }
 
+func consumeCmd(c *exec.Cmd) error {
+	if err := c.Start(); err != nil {
+		return err
+	}
+
+	if err := c.Wait(); err != nil {
+		return err
+	}
+
+	if code := c.ProcessState.ExitCode(); code != 0 {
+		return fmt.Errorf("process %v exit with code %d", c.Path, code)
+	}
+	return nil
+}
+
 func output(cmd0 string, args ...string) (string, error) {
 	cmd := exec.Command(cmd0, args...)
 
 	var stdOut = bytes.NewBuffer(nil)
 	cmd.Stdout = stdOut
 
-	if err := cmd.Start(); err != nil {
+	if err := consumeCmd(cmd); err != nil {
 		return "", err
+	} else {
+		return stdOut.String(), nil
 	}
-
-	if err := cmd.Wait(); err != nil {
-		return "", err
-	}
-
-	return stdOut.String(), nil
 }
 
 func errorOutput(cmd0 string, args ...string) (string, error) {
@@ -37,13 +49,9 @@ func errorOutput(cmd0 string, args ...string) (string, error) {
 	var errOut = bytes.NewBuffer(nil)
 	cmd.Stderr = errOut
 
-	if err := cmd.Start(); err != nil {
+	if err := consumeCmd(cmd); err != nil {
 		return "", err
+	} else {
+		return errOut.String(), nil
 	}
-
-	if err := cmd.Wait(); err != nil {
-		return "", err
-	}
-
-	return errOut.String(), nil
 }
