49d5c1639f7857f835526074065fc9583e9fd536
Myriad-Dreamin <camiyoru@gmail.com>
2022-03-29 20:15:57 +0800

    dev(judge): provide judge client adaptor to service

---
+++ pkg/tarus-judge/adaptor.go
@@ -0,0 +1,56 @@
+package tarus_judge
+
+import (
+	"context"
+	"github.com/Myriad-Dreamin/tarus/api/tarus"
+	emptypb "google.golang.org/protobuf/types/known/emptypb"
+)
+
+func NewClientAdaptor(c tarus.JudgeServiceClient) tarus.JudgeServiceServer {
+	return &ClientAdaptor{c: c}
+}
+
+type ClientAdaptor struct {
+	tarus.UnimplementedJudgeServiceServer
+	c tarus.JudgeServiceClient
+}
+
+func (c *ClientAdaptor) Handshake(ctx context.Context, request *tarus.HandshakeRequest) (*tarus.HandshakeResponse, error) {
+	return c.c.Handshake(ctx, request)
+}
+
+func (c *ClientAdaptor) CreateContainer(ctx context.Context, request *tarus.CreateContainerRequest) (*emptypb.Empty, error) {
+	return c.c.CreateContainer(ctx, request)
+}
+
+func (c *ClientAdaptor) BundleContainer(ctx context.Context, request *tarus.BundleContainerRequest) (*emptypb.Empty, error) {
+	return c.c.BundleContainer(ctx, request)
+}
+
+func (c *ClientAdaptor) RemoveContainer(ctx context.Context, request *tarus.RemoveContainerRequest) (*emptypb.Empty, error) {
+	return c.c.RemoveContainer(ctx, request)
+}
+
+func (c *ClientAdaptor) CloneContainer(ctx context.Context, request *tarus.CloneContainerRequest) (*emptypb.Empty, error) {
+	return c.c.CloneContainer(ctx, request)
+}
+
+func (c *ClientAdaptor) CheckContainer(ctx context.Context, request *tarus.CheckContainerRequest) (*emptypb.Empty, error) {
+	return c.c.CheckContainer(ctx, request)
+}
+
+func (c *ClientAdaptor) CopyFile(ctx context.Context, request *tarus.CopyRequest) (*emptypb.Empty, error) {
+	return c.c.CopyFile(ctx, request)
+}
+
+func (c *ClientAdaptor) CompileProgram(ctx context.Context, request *tarus.CompileProgramRequest) (*emptypb.Empty, error) {
+	return c.c.CompileProgram(ctx, request)
+}
+
+func (c *ClientAdaptor) MakeJudge(ctx context.Context, request *tarus.MakeJudgeRequest) (*tarus.MakeJudgeResponse, error) {
+	return c.c.MakeJudge(ctx, request)
+}
+
+func (c *ClientAdaptor) QueryJudge(ctx context.Context, request *tarus.QueryJudgeRequest) (*tarus.QueryJudgeResponse, error) {
+	return c.c.QueryJudge(ctx, request)
+}
